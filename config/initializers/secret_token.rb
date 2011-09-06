25a7edc244a4fd987c39c94daff13d9a256255e2
Daniel Szmulewicz <danielsz@sdf.lonestar.org>
2011-09-06 05:05:10 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Merjerz::Application.config.secret_token = 'eed785effba5384d2e0cb0b7242bff3f210ae61a5413575e5639c0302ddd32f1fd7eb91b30c3dbe9d512147036ab842f930829716a411deec0be2139194bfcc7'
